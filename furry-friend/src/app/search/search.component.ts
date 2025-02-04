4974c796b2b00703218cae21ec6b9a0627cccee7
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-03 18:44:07 -0600

    add table to search component

--- furry-friend/src/app/search/search.component.ts
+++ furry-friend/src/app/search/search.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { CardComponent } from '../card/card.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-search',
-  imports: [HeaderComponent, CardComponent],
+  imports: [HeaderComponent, CardComponent, TableComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
