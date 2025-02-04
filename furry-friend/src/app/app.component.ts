98ae1931324ee539722092c06a8bb6b5edb63ef0
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-03 18:33:30 -0600

    add table component

--- furry-friend/src/app/app.component.ts
+++ furry-friend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SearchComponent } from './search/search.component';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SearchComponent],
+  imports: [RouterOutlet, LoginComponent, SearchComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ furry-friend/src/app/table/table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-table',
+  imports: [CardComponent],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css'
+})
+export class TableComponent {
+
+}
