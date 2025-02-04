7b0d4db97c232c415d66ce95edd358aa5f0a0fd8
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-03 21:57:12 -0600

    add routes

--- furry-friend/src/app/app.routes.ts
+++ furry-friend/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
 
+export const routes: Routes = [
+    {path: 'login', component:LoginComponent }, 
+    {path: 'search', component:SearchComponent }, 
 
-export const routes: Routes = [];
+];
--- furry-friend/src/app/table/table.component.ts
+++ furry-friend/src/app/table/table.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-table',
-  imports: [CardComponent],
+  imports: [NgFor, CardComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-
 }
