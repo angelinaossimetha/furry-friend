be662ec087795087e264639d36bcd73ed6ba4817
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-07 22:51:24 -0600

    add match component

--- furry-friend/src/app/app.component.ts
+++ furry-friend/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { SearchComponent } from './search/search.component';
 import { TableComponent } from './table/table.component';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { MatchComponent } from './match/match.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SearchComponent, TableComponent, FavoritesComponent],
+  imports: [RouterOutlet, LoginComponent, SearchComponent, TableComponent, FavoritesComponent, MatchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ furry-friend/src/app/match/match.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-match',
+  imports: [HeaderComponent, CardComponent],
+  templateUrl: './match.component.html',
+  styleUrl: './match.component.css'
+})
+export class MatchComponent {
+
+}
