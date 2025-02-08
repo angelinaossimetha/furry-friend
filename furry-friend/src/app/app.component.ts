a0afef5881fadd5723915d607e9b42e5b912d90e
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-07 22:38:54 -0600

    add favorites component

--- furry-friend/src/app/app.component.ts
+++ furry-friend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SearchComponent } from './search/search.component';
 import { TableComponent } from './table/table.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SearchComponent, TableComponent],
+  imports: [RouterOutlet, LoginComponent, SearchComponent, TableComponent, FavoritesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ furry-friend/src/app/favorites/favorites.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { TableComponent } from '../table/table.component';
+@Component({
+  selector: 'app-favorites',
+  imports: [HeaderComponent, TableComponent ],
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.css'
+})
+export class FavoritesComponent {
+
+}
