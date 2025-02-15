a0edb8f5efda1dc63f55a17f175e958a6a65f80f
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-14 21:07:43 -0600

    Add footer

--- furry-friend/src/app/login/login.component.ts
+++ furry-friend/src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
-  imports: [HeaderComponent, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
