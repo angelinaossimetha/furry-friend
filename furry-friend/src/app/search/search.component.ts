b8018791b4d5c454f4fea5bac9a4299faccdc4eb
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-18 00:02:36 -0600

    Add filter component

--- furry-friend/src/app/search/search.component.ts
+++ furry-friend/src/app/search/search.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { CardComponent } from '../card/card.component';
 import { TableComponent } from '../table/table.component';
+import { FilterComponent } from '../filter/filter.component';
+
 
 @Component({
   selector: 'app-search',
-  imports: [HeaderComponent, CardComponent, TableComponent],
+  imports: [HeaderComponent, CardComponent, TableComponent, FilterComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
