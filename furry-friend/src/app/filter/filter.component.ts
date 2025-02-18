40f6191e3d6c72d378a4c298a07869f814df9e6c
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-18 00:38:50 -0600

    update filter

--- furry-friend/src/app/filter/filter.component.ts
+++ furry-friend/src/app/filter/filter.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import * as breedsData from '../../../public/breeds.json'
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [ NgFor],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  breeds: string[] = breedsData.breeds; 
 
 }
