079119fb14f8c2904f43c6e8af3ed5e7da7d3184
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-02-28 16:07:45 -0600

    add match functionality

--- furry-friend/src/app/match/match.component.ts
+++ furry-friend/src/app/match/match.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { CardComponent } from '../card/card.component';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-match',
-  imports: [HeaderComponent, CardComponent],
+  imports: [HeaderComponent, CardComponent, CommonModule],
   templateUrl: './match.component.html',
   styleUrl: './match.component.css'
 })
 export class MatchComponent {
   isVisibleLogoutBtn: boolean = true; 
+  match: any = {}; 
+  hasFavorites: boolean = false;
+
+  constructor() { 
+    var favorites: any = localStorage.getItem('favorites');
+    favorites = JSON.parse(favorites)
+    if (favorites.length === 0) return; 
+    this.hasFavorites = true;
+    
+    
+
+    const matchId: number = Math.floor(Math.random() * favorites.length); 
+
+    this.match = favorites[matchId];
+
+  }
 }
