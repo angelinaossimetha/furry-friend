8e178c5e2c2c32cc53c28536d0fbfddd191e9869
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-03-01 16:08:32 -0600

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: 'index.html',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: 'index.html',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 27878, hash: 'e81094254974b49e2aa07cbe6a9873c2fb4c8ea2ad2ed8aea1712ab68dc0f28d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17181, hash: '51814f0df3239b2da6155c2701d0497312af41770f010880062c73790bc4f1f1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27887, hash: '5c079ed4d801fbcf9a52d4bf5733a3af03b72ef97c6c02fe821824778d520a19', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17190, hash: '1040810fb4d7202a9a89dd3382c5e29da67838efff8a2019eec7ba2978c5843d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-7JS7GIZR.css': {size: 238590, hash: 'BLck8M/le/E', text: () => import('./assets-chunks/styles-7JS7GIZR_css.mjs').then(m => m.default)}
   },
 };
