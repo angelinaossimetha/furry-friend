90410206e89976ddb6242fe48d9f98ca3180f63b
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-03-01 16:48:15 -0600

    update angular json

--- furry-friend/docs/server/angular-app-manifest.mjs
+++ furry-friend/docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 27913, hash: '8ba90c3d4e3ac34e66375e0aacca227d7364130d974409c74e483388544dec2b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17216, hash: 'c7fc66c54e7dbd9c9019ecf5d6764c735c6f9b8c0fe007d68106361ace046bc6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27913, hash: '3758187d4fc57f4857bd3158598834834255055b44f6dbbb6efd9213f4515a10', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17216, hash: '197137d6294250ece47bea8eeef150a7f5970f8dc4e2cfc124917a506f55a99d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-7JS7GIZR.css': {size: 238590, hash: 'BLck8M/le/E', text: () => import('./assets-chunks/styles-7JS7GIZR_css.mjs').then(m => m.default)}
   },
 };
