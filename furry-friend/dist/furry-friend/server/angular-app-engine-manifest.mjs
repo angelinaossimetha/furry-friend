424f67121009fd4bb72c6a1e0abfa63cd5bba8ed
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-03-02 19:46:54 -0600

    change href path

--- furry-friend/dist/furry-friend/server/angular-app-engine-manifest.mjs
+++ furry-friend/dist/furry-friend/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: 'https://angelinaossimetha.github.io/furry-friend',
   supportedLocales: {
   "en-US": ""
 },
--- furry-friend/dist/furry-friend/server/angular-app-manifest.mjs
+++ furry-friend/dist/furry-friend/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: 'https://angelinaossimetha.github.io/furry-friend/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 27887, hash: 'ed1a15066b9f5c468d4c31300b5d018dcef4f2ee8eb218075a18bdb64a274368', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17190, hash: '2f30f86d2f1293fd6ca59bcf16f25382ac65703cd4d89b55b3d0dab5d44ae95c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27928, hash: '7c5cfe875e5e1db45d536c8252b99f7a4028ec5c236297477240afdfee2dcc4a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17231, hash: '4102ffd48f02198582eab51d5e8e7bc2e8b09e1cd64b326ff924216785789090', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-7JS7GIZR.css': {size: 238590, hash: 'BLck8M/le/E', text: () => import('./assets-chunks/styles-7JS7GIZR_css.mjs').then(m => m.default)}
   },
 };
