ab1a3afa34228b9d5381c565511e01160e8d293e
Angelina Ossimetha <angelinaossimetha@gmail.com>
2025-03-02 19:12:45 -0600

    ran ng deploy --build-target=prod --cname=https://angelinaossimetha.github.io/furry-friend/

--- furry-friend/dist/furry-friend/server/angular-app-manifest.mjs
+++ furry-friend/dist/furry-friend/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/furry-friend/',
+  baseHref: '/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 27913, hash: 'ccc65c93b857a308794b775ea671a691685d8fb162e7cb9da88d53dc7ba6b161', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17216, hash: 'cc87840c2994dc398283b9d72d008b94f2d5918ca1f233bda01075ee14abb569', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27887, hash: 'ed1a15066b9f5c468d4c31300b5d018dcef4f2ee8eb218075a18bdb64a274368', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17190, hash: '2f30f86d2f1293fd6ca59bcf16f25382ac65703cd4d89b55b3d0dab5d44ae95c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-7JS7GIZR.css': {size: 238590, hash: 'BLck8M/le/E', text: () => import('./assets-chunks/styles-7JS7GIZR_css.mjs').then(m => m.default)}
   },
 };
